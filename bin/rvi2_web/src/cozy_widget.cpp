ee18b4d74ca82cfbcd3218004f540d774186776b
Ian A.B. King <iansens@hotmail.com>
2019-01-23 19:02:07 +0100

    move gl methods to .cpp

--- bin/rvi2_web/src/cozy_widget.cpp
+++ bin/rvi2_web/src/cozy_widget.cpp
@@ -10,11 +10,24 @@ const std::string cozy_widget::VX_SHADER_PATH = "shaders/vxshader.glsl";
 cozy_widget::cozy_widget()
 {
     resize(800, 600);
-    decorationStyle().setBackgroundColor(Wt::WColor(32,32,32));
+    decorationStyle().setBackgroundColor(Wt::WColor(32, 32, 32));
 
     _client_id = _runtime.create_client();
     _runtime.start_client(_client_id);
-    _runtime.get_instance(_client_id).create_binding("hello_click",&hello_click);
+    _runtime.get_instance(_client_id).create_binding("hello_click", 
+    [](rvi::client_instance& inst, const rvi::arglist_t& args) {
+        // Save current selected frame
+        auto& ctx = inst.context;
+        rvi::frame* save_ptr = ctx.selected_frame();
+
+        // Return to root frame
+        ctx.select_frame("hello_friend");
+        ctx.clear_frame();
+        ctx.clear_children();
+        ctx.set_position(rvi::vector2(0.10f, 0.15f));
+        rvi::std_bindings::printx(inst, ctx.selected_frame()->name() , "+-+-+ HELLO FRIEND +-+-+");
+        ctx.select_frame(save_ptr);
+    });
 
     this->clicked().connect([&](const Wt::WMouseEvent& e)
     { 
@@ -103,4 +116,26 @@ void cozy_widget::refresh_snapshot()
     enableVertexAttribArray(AttribLocation(0));
     bindAttribLocation(_sh_program, 0, "vertex");
     bufferDatafv(ARRAY_BUFFER, _vx_buff.begin(), _vx_buff.end(), DYNAMIC_DRAW);
+}
+
+void cozy_widget::initializeGL()
+{
+    compile_shaders();
+    _vbo = createBuffer();
+    _gl_initialized = true;
+    refresh_snapshot();
+}
+
+void cozy_widget::resizeGL(int width, int height)
+{
+    this->viewport(0, 0, width, height);
+}
+
+void cozy_widget::paintGL()
+{
+    refresh_snapshot();
+    clearColor(0.2, 0.2, 0.2, 0.5);
+    useProgram(_sh_program);
+    bindBuffer(ARRAY_BUFFER, _vbo);
+    drawArrays(LINES, 0, _vx_buff.size() / 3);
 }
--- bin/rvi2_web/src/cozy_widget.hpp
+++ bin/rvi2_web/src/cozy_widget.hpp
@@ -7,21 +7,6 @@
 #include <rvi/runtime.hpp>
 #include <rvi/std_bindings.hpp>
 
-static void hello_click(rvi::client_instance& inst, const rvi::arglist_t&)
-{
-    // Save current selected frame
-    auto& ctx = inst.context;
-    rvi::frame* save_ptr = ctx.selected_frame();
-
-    // Return to root frame
-    ctx.select_frame("hello_friend");
-    ctx.clear_frame();
-    ctx.clear_children();
-    ctx.set_position(rvi::vector2(0.10f, 0.15f));
-    rvi::std_bindings::printx(inst, ctx.selected_frame()->name() , "+-+-+ HELLO FRIEND +-+-+");
-    ctx.select_frame(save_ptr);
-}
-
 class cozy_widget : public Wt::WGLWidget
 {
 private:
@@ -41,32 +26,8 @@ public:
     cozy_widget();
     void refresh_snapshot();
 
-    void initializeGL() override
-    {
-        compile_shaders();
-        _vbo = createBuffer();
-        _gl_initialized = true;
-        refresh_snapshot();
-    }
-
-    void resizeGL(int width, int height) override
-    {
-        std::cout << "resizeGL()" << std::endl;
-        this->viewport(0, 0, width, height);
-    }
-
-    void paintGL() override
-    {
-        refresh_snapshot();
-        std::cout << "paintGL()" << std::endl;
-		clearColor(0.2, 0.2, 0.2, 0.5);
-        useProgram(_sh_program);
-        bindBuffer(ARRAY_BUFFER, _vbo);
-        drawArrays(LINES, 0, _vx_buff.size() / 3);
-    }
-
-    void updateGL() override
-    {
-        std::cout << "updateGL()" << std::endl;
-    }
+    void initializeGL() override;
+    void resizeGL(int width, int height) override;
+    void paintGL() override;
+    void updateGL() override {}
 };
